1d57bd70421a2dec4278a4e09f78f08b74ed3a70
Sadisha Nimsara <nsadisha@gmail.com>
2022-09-14 14:42:58 +0530

    moving snake

--- js/Snake.js
+++ js/Snake.js
@@ -4,10 +4,14 @@ export default class Snake {
     #length = 1
     #speed
     #snakeHead
+    direction = 'right'
+    moving = null
     
     constructor(speed){
         this.#speed = speed
         this.#snakeHead = new SnakeCell(this.#length)
+
+        this.moving = setInterval(this.move.bind(this), speed)
     }
 
     getSnake(){
@@ -15,6 +19,21 @@ export default class Snake {
     }
 
     move(){
-        console.log(78);
+        var currentX = this.#snakeHead.cellX
+        var currentY = this.#snakeHead.cellY
+
+        if(this.direction == 'up'){
+            this.#snakeHead.updatePosition(currentX, currentY-1)
+        }else if(this.direction == 'down'){
+            this.#snakeHead.updatePosition(currentX, currentY+1)
+        }else if(this.direction == 'left'){
+            this.#snakeHead.updatePosition(currentX-1, currentY)
+        }else if(this.direction == 'right'){
+            this.#snakeHead.updatePosition(currentX+1, currentY)
+        }
+    }
+
+    setDirection(direction){
+        this.direction = direction
     }
 }
