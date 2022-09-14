4265c3db8e2270079fa5202c9942b6afedafd2ff
Sadisha Nimsara <nsadisha@gmail.com>
2022-09-14 22:43:24 +0530

    food class

--- js/Food.js
+++ js/Food.js
@@ -1,5 +1,26 @@
-class Food {
+export default class Food {
+    _x
+    _y
+    #food
+
     constructor(x, y){
-        
+        this._x = x
+        this._y = y
+        this.#food = document.createElement('div')
+        this.#food.classList.add('food')
+        this.#food.setAttribute('style', `--row: ${y}; --col: ${x};`)
+    }
+
+    getFood(){
+        return this.#food
+    }
+    remove(){
+        this.#food.remove()
+    }
+    getPosition(){
+        return{
+            x: this._x,
+            y: this._y
+        }
     }
 }
