925c92ea8718c2702031c297ad0ce521ff3ad0f6
Sadisha Nimsara <nsadisha@gmail.com>
2022-09-17 22:46:17 +0530

    start game and init callback functions

--- js/script.js
+++ js/script.js
@@ -2,4 +2,17 @@ import SnakeGame from "./SnakeGame.js"
 
 const game = new SnakeGame()
 
-game.start()
+const startBtn = document.getElementById('start-btn')
+const startScreen = document.querySelector('.start-screen')
+
+startBtn.addEventListener('click', e => {
+    startScreen.classList.add('scale-down')
+    game.start()
+})
+
+game.onGameOver = function(){
+    console.log('game over');
+}
+game.onRestart = function(){
+    console.log("restarted");
+}
